b9d95092c8a06a07e20136417366cd06ae26dfed
Artem <temkaea@gmail.com>
2025-03-12 15:56:47 +0700

    Add Prescaller

--- Test_Core0/src/SystClock.asm
+++ Test_Core0/src/SystClock.asm
@@ -1,20 +1,19 @@
 #include "defBF607.h"
 #include "SystClock.h"
 
+
 // R0 - Опрашиваемый Регистр
 // R1 - Значение маски
-#define  PLL_REGS_CHECK()			R1 = R0 & R1; \
-									R1 = R0 | R1 
-#define  PLL_REGS_CHECK_(Reg1, Reg2)			Reg1 = Reg1 & Reg2; \
-												Reg1 = Reg1 | Reg2 
-
+#define  REG_SET_MASKOR(Reg1, Reg2)			Reg1 = Reg1 & Reg2; //OR MASK										
+#define  REG_SET_MASKANDK(Reg1, Reg2)		Reg1 = Reg1 | Reg2; //AND MASK
+												
 .GLOBAL _SystClock;	
 .SECTION program
 .ALIGN 4;
 _SystClock:
 _SystClock.init:
 	//Проверка текущего состояния CGU_STAT:
-	 CALL Check_temp_SystClock;
+	 CALL _Check_temp_SystClock;
 	 	
 	//Настройка коэффициента деления:
 	 CALL _Set_Kofficient;
@@ -28,88 +27,41 @@ _SystClock.error:
 	RTS; 
 _SystClock.end:
 
-//============================================================================
-//Функция проверки текущего состояния CGU_STAT
-.GLOBAL Check_temp_SystClock;	
-.SECTION program
-.ALIGN 4;
-Check_temp_SystClock:
-     P0.L =  LO(REG_CGU0_STAT);	
-     P0.H =  HI(REG_CGU0_STAT);
-//Регистр будет хранится в R0    
-	 R0 = [P0];
-//Проверка текущего состояния CGU_STAT:	 
-	 [--SP] = R0;
-	 R1.L = LO(BITM_CGU_STAT_PLLEN);
-	 R1.H = HI(BITM_CGU_STAT_PLLEN);
-	 R2 = 1;
-	 CALL _Mask_Add;
-	 R0 = [SP++];
-	 
-	 PLL_REGS_CHECK();
-	 
-	 PLL_REGS_CHECK_(R0, R1);
-	 
-	 [--SP] = R0;
-	 R1 = BITM_CGU_STAT_PLOCK;
-	 R2 = 1;
-	 CALL _Mask_Add;
-	 R0 = [SP++];
-	 
-	 [--SP] = R0;
-	 R1.L = LO(BITM_CGU_STAT_PLOCKERR);
-	 R1.H = HI(BITM_CGU_STAT_PLOCKERR);
-	 R2 = 1;
-	 CALL _Mask_Add;
-	 R0 = [SP++];
-	 
-	 [--SP] = R0;
-	 R1.L = LO(BITM_CGU_STAT_CLKSALGN);
-	 R1.H = HI(BITM_CGU_STAT_CLKSALGN);
-	 R2 = 0;
-	 CALL _Mask_Add;
-	 R0 = [SP++];
-	 
-	 RTS;
-Check_temp_SystClock.end:
+
 
 //============================================================================
-.GLOBAL Set_Kofficient;	
+.GLOBAL _Set_Kofficient;	
 .SECTION program
 .ALIGN 4;
-Set_Kofficient:
-
-Set_Kofficient.end:
-//============================================================================
-//Соглашение:
-// R0 - Опрашиваемый Регистр
-// R1 - Значение маски
-// R2 - Вид Операции[1 -> OR MASK] [0 -> AND MASK] 
-// Возвращает код ошибки
-.GLOBAL _Mask_Add;	
-.SECTION program
-.ALIGN 4;
-_Mask_Add:
-	R3 = R1;
-	CC = R2;
-	IF CC JUMP _Mask_Add.OR;
-Mask_Add.AND:	
-	R1 = R0 & R1; 
-	CC = R0 == R1; 
-	IF !CC JUMP _Mask_Add.False;
+_Set_Kofficient:
+	P0.L =  LO(REG_CGU0_CTL);	
+    P0.H =  HI(REG_CGU0_CTL);
+    
+    R0 = [P0];
+    BITCLR(R0, BITP_CGU_CTL_MSEL);	
+    R0 = R0 | 20;
+    
+    [P0] = R0; 
+_Set_Kofficient.wait:
+	P0.L = LO(REG_CGU0_STAT);
+	P0.L = LO(REG_CGU0_STAT);
+	R0 = [P0];
 	
-	JUMP _Mask_Add.True;
-_Mask_Add.OR:	
-	R1 = R0 | R1; 
-	CC = R0 == R1; 
-	IF !CC JUMP _Mask_Add.False;
+	//Ожидание фиксации бита PLOCK = 1
+	CC = BITTST(R0, BITP_CGU_STAT_PLOCK );
+	IF CC JUMP _Set_Kofficient.wait;
 	
-_Mask_Add.True:
-	RTS;
-_Mask_Add.False:
+	//Установка делителей:
+	 P0.L = LO(CGU_DIV);
+     P0.H = HI(CGU_DIV);
+     R0 = [P0];
 	
-_Mask_Add.end:
-
+	 BITCLR(R0, BITP_CGU_DIV_SYSSEL);
+	 R0 = R0 | (4 << 4);
+	 [P0] = R0; 
+	 
+	
+_Set_Kofficient.end:
 
 
 
