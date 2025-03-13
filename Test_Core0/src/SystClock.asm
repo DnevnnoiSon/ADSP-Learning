eeb8cc5233103192883ea2075f4df223990f6009
Artem <temkaea@gmail.com>
2025-03-13 15:29:02 +0700

    Prescaller Config/Finished

--- Test_Core0/src/SystClock.asm
+++ Test_Core0/src/SystClock.asm
@@ -4,8 +4,8 @@
 
 // R0 - Опрашиваемый Регистр
 // R1 - Значение маски
-#define  REG_SET_MASKOR(Reg1, Reg2)			Reg1 = Reg1 & Reg2; //OR MASK										
-#define  REG_SET_MASKANDK(Reg1, Reg2)		Reg1 = Reg1 | Reg2; //AND MASK
+#define  REG_SET_MASKOR(Reg1, Reg2)			Reg1 = Reg1 & Reg2 //OR MASK										
+#define  REG_SET_MASKANDK(Reg1, Reg2)		Reg1 = Reg1 | Reg2 //AND MASK
 												
 .GLOBAL _SystClock;	
 .SECTION program
@@ -13,12 +13,17 @@
 _SystClock:
 _SystClock.init:
 	//Проверка текущего состояния CGU_STAT:
+	 R0 = 1;
 	 CALL _Check_temp_SystClock;
 	 	
 	//Настройка коэффициента деления:
 	 CALL _Set_Kofficient;
 	 
     //Изменение частоты PLL:
+    
+    //Проверка завершения процесса:
+     R0 = 0;
+    CALL _Check_temp_SystClock;
 	 RTS;
 _SystClock.error:
 //Опрос кодов ошибок
@@ -28,8 +33,50 @@ _SystClock.error:
 _SystClock.end:
 
 
+//================= ПРОВЕРКА ТЕКУЩЕГО СОСТОЯНИЯ ===================
+//СОГЛАШЕНИЕ: R0 - ЭЛЕМЕНТ ОТВЕЧАЮЩИЙ ЗА НАЛОЖЕНИЕ МАСКИ
+.GLOBAL _Check_temp_SystClock;	
+.SECTION program
+.ALIGN 4;
+_Check_temp_SystClock:
+	R3 = R0;
+    P0.L =  LO(REG_CGU0_STAT);	
+    P0.H =  HI(REG_CGU0_STAT);
 
-//============================================================================
+	R0 = [P0];
+
+	R1.L = LO(BITM_CGU_STAT_PLLEN);	
+	R1.h = HI(BITM_CGU_STAT_PLLEN);
+	REG_SET_MASKANDK(R0, R1);
+
+	R1.L = LO(BITM_CGU_STAT_PLOCK);
+	R1.H = HI(BITM_CGU_STAT_PLOCK);
+	REG_SET_MASKANDK(R0, R1);
+	
+	R1 = 0(Z);
+	CC = R3 == R1;
+	IF CC JUMP _Check_temp_SystClock.PLLBP;
+_Check_temp_SystClock.PLOCKERR:
+	R1.L = LO(BITM_CGU_STAT_PLOCKERR);	
+	R1.H = HI(BITM_CGU_STAT_PLOCKERR);	
+	REG_SET_MASKANDK(R0, R1);
+	
+	JUMP _Check_temp_SystClock.CLKSALGN;
+_Check_temp_SystClock.PLLBP:
+	R1.L = LO(BITM_CGU_STAT_PLLBP);	
+	R1.H = HI(BITM_CGU_STAT_PLLBP);	
+	REG_SET_MASKANDK(R0, R1);
+
+_Check_temp_SystClock.CLKSALGN:
+	R1.L = LO(BITM_CGU_STAT_CLKSALGN);	
+	R1.H = HI(BITM_CGU_STAT_CLKSALGN);	
+	REG_SET_MASKOR(R0, R1);
+	
+	[P0] = R0;
+	RTS;
+_Check_temp_SystClock.end:
+
+//================ НАТСРОЙКА ЧАСТОТЫ ДЕЛЕНИЯ ===================
 .GLOBAL _Set_Kofficient;	
 .SECTION program
 .ALIGN 4;
@@ -39,28 +86,43 @@ _Set_Kofficient:
     
     R0 = [P0];
     BITCLR(R0, BITP_CGU_CTL_MSEL);	
-    R0 = R0 | 20;
+    
+    R1 = 0x14;
+    R0 = R0 | R1;
     
     [P0] = R0; 
-_Set_Kofficient.wait:
+_Set_Kofficient.PLLwait:
 	P0.L = LO(REG_CGU0_STAT);
-	P0.L = LO(REG_CGU0_STAT);
+	P0.H = LO(REG_CGU0_STAT);
 	R0 = [P0];
 	
 	//Ожидание фиксации бита PLOCK = 1
 	CC = BITTST(R0, BITP_CGU_STAT_PLOCK );
-	IF CC JUMP _Set_Kofficient.wait;
+	IF CC JUMP _Set_Kofficient.PLLwait;
 	
 	//Установка делителей:
-	 P0.L = LO(CGU_DIV);
-     P0.H = HI(CGU_DIV);
+	 P0.L = LO(REG_CGU0_DIV);
+     P0.H = HI(REG_CGU0_DIV);
      R0 = [P0];
 	
 	 BITCLR(R0, BITP_CGU_DIV_SYSSEL);
-	 R0 = R0 | (4 << 4);
+	 R1 = 0 (Z);
+	 R1 = (4 << 4);
+	 R0 = R0 | R1;
 	 [P0] = R0; 
-	 
 	
+_Set_Kofficient.Align_Wait:
+	P0.L = LO(REG_CGU0_STAT);
+	P0.L = LO(REG_CGU0_STAT);
+	R0 = [P0];
+//ожидание сигнализации тактовых сигналов 
+	R1 = (1 << 8);
+   	R0 = R0 & R1;  // Проверяем бит CLKSALGN
+    R1 = 0(Z);
+  	CC = R0 == R1;
+  	IF CC JUMP _Set_Kofficient.Align_Wait;
+
+	RTS;
 _Set_Kofficient.end:
 
 
