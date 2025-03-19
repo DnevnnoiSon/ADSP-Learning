1e030efd5fdff10bce326a8e9901768f4f58c11f
Artem <temkaea@gmail.com>
2025-03-19 18:45:31 +0700

    Добавил функцию настройки частоты(делитель|множитель)

--- Test_Core0/src/SystClock.asm
+++ Test_Core0/src/SystClock.asm
@@ -1,6 +1,10 @@
 #include "defBF607.h"
 #include "SystClock.h"
 
+.GLOBAL _Check_temp_SystClock;
+.GLOBAL _Set_Kofficient;
+.GLOBAL _Change_PLL_Frequency;
+
 // R0 - Опрашиваемый Регистр
 // R1 - Значение маски
 #define  REG_SET_MASKAND(Reg1, Reg2) 	Reg2 = Reg1 & Reg2  //AND MASK								
@@ -94,114 +98,52 @@ _Check_temp_SystClock.end:
 .SECTION program
 .ALIGN 4;
 _Set_Kofficient:
+//============= Отключение PLL =======================
+	P0.L =  LO(REG_CGU0_CTL);	
+    P0.H =  HI(REG_CGU0_CTL);
+   
+    R0 = [P0];
+    BITCLR(R0, BITP_CGU_STAT_PLLEN);	
+    [P0] = R0; 
+//=== Установка коэффициентов деления/множителей =====
+	P0.L = LO(REG_CGU0_DIV);
+    P0.H = HI(REG_CGU0_DIV);
+	R0 = [P0];
+	//Сгенерить:
+	R1 = ((40 << BITP_CGU_CTL_MSEL)
+	    | ( 2 << BITP_CGU_CTL_DF)
+	    | ( 1 << BITP_CGU_DIV_CSEL));
+	R0 = R0 | R1; 
+	[P0] = R0; 
+//============= Включение PLL =======================	
 	P0.L =  LO(REG_CGU0_CTL);	
     P0.H =  HI(REG_CGU0_CTL);
     
     R0 = [P0];
-    BITCLR(R0, BITP_CGU_CTL_MSEL);	
+    BITSET(R0, BITP_CGU_STAT_PLLEN);	
+    [P0] = R0;
+//============= Ожидание Стабилизации ===============
+_Set_Kofficient.WaitPLOCK:
+    P0.L = LO(REG_CGU0_STAT);
+    P0.H = HI(REG_CGU0_STAT);
+    R0 = [P0];
     
-    R1 = 0x14;
-    R0 = R0 | R1;
+    R1.L = LO(BITM_CGU_STAT_PLOCK);
+    R1.H = HI(BITM_CGU_STAT_PLOCK);
+    REG_SET_MASKOR(R0, R1);
+    CC = R0 == R1;
+//Сброс на начало ожидания:
+    IF !CC JUMP _Set_Kofficient.WaitPLOCK;
     
-    [P0] = R0; 
-_Set_Kofficient.PLLwait:
-	P0.L = LO(REG_CGU0_STAT);
-	P0.H = LO(REG_CGU0_STAT);
-	R0 = [P0];
-	
-	//Ожидание фиксации бита PLOCK = 1
-	CC = BITTST(R0, BITP_CGU_STAT_PLOCK );
-	IF CC JUMP _Set_Kofficient.PLLwait;
-	
-	//Установка делителей:
-	 P0.L = LO(REG_CGU0_DIV);
-     P0.H = HI(REG_CGU0_DIV);
-     R0 = [P0];
-	
-	 BITCLR(R0, BITP_CGU_DIV_SYSSEL);
-	 R1 = 0 (Z);
-	 R1 = (4 << 4);
-	 R0 = R0 | R1;
-	 [P0] = R0; 
-	
-_Set_Kofficient.Align_Wait:
-	P0.L = LO(REG_CGU0_STAT);
-	P0.L = LO(REG_CGU0_STAT);
-	R0 = [P0];
-//ожидание сигнализации тактовых сигналов 
-	R1 = (1 << 8);
-   	R0 = R0 & R1;  // Проверяем бит CLKSALGN
-    R1 = 0(Z);
-  	CC = R0 == R1;
-  	IF CC JUMP _Set_Kofficient.Align_Wait;
-  	
 	RTS;
 _Set_Kofficient.end:
 
 //================ ИЗМЕНЕНИЕ ЧАСТОТЫ PLL ===================
-.GLOBAL _Change_PLL_Frequency
+.GLOBAL _Change_PLL_Frequency;
 .SECTION program
 .ALIGN 4;
 _Change_PLL_Frequency:
-    P0.L = LO(REG_CGU0_STAT);
-    P0.H = HI(REG_CGU0_STAT);
-    R0 = [P0];
-
-    //Отключение режима байпаса PLL:
-    BITCLR(R0, BITP_CGU_STAT_PLOCKERR); 
-    [P0] = R0;  
-
-
-    P0.L = LO(REG_CGU0_CTL);
-    P0.H = HI(REG_CGU0_CTL);
-    R0 = [P0];
-
-    R1.L = LO(BITM_CGU_CTL_PLLBP);
-    R1.H = HI(BITM_CGU_CTL_PLLBP);
-    R0 = R0 | R1; 
-
-    [P0] = R0;  
-
-    P0.L = LO(REG_CGU0_CTL);
-    P0.H = HI(REG_CGU0_CTL);
-    R0 = [P0];
-
-    R1 = 0x3F;  
-    R1 = ~R1;
-    R0 = R0 & R1; 
-
-    R1 = 0x14; 
-    R0 = R0 | R1;
-
-    [P0] = R0;  
-   
-    P0.L = LO(REG_CGU0_CTL);
-    P0.H = HI(REG_CGU0_CTL);
-    R0 = [P0];
-
-    BITCLR(R0, BITP_CGU_STAT_PLLBP); 
-
-    [P0] = R0;  
-//Ожидание, пока PLL заблокируется:
-_PLL_Wait_Lock:
-    P0.L = LO(REG_CGU0_STAT);
-    P0.H = HI(REG_CGU0_STAT);
-    R0 = [P0];
-
-    CC = BITTST(R0, BITP_CGU_STAT_PLOCK);
-    IF !CC JUMP _PLL_Wait_Lock;  
- //Ожидание выравнивания PLL:
-_PLL_Wait_Align:
-    P0.L = LO(REG_CGU0_STAT);
-    P0.H = HI(REG_CGU0_STAT);
-    R0 = [P0];
-
-    R1 = (1 << 8);
-    R0 = R0 & R1;  
-    R1 = 0(Z);
-    CC = R0 == R1;
-    IF CC JUMP _PLL_Wait_Align;
-
+ 
     RTS; 
 _Change_PLL_Frequency.end:
 
